eef597cba4d8b81635326dc794af3209bdadc7e8
4trustja-design <4trustja@gmail.com>
2026-05-04 23:45:57 +0200

    Update model version to 'gemini-1.5-flash'

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ try:
     genai.configure(api_key=API_KEY)
     
     # Pokušavamo sa "gemini-1.5-flash-latest" što je najsigurnija putanja
-    model = genai.GenerativeModel('gemini-1.0-pro') 
+    model = genai.GenerativeModel('gemini-1.5-flash') 
 except Exception as e:
     st.error(f"Greška pri konfiguraciji: {e}")
 
